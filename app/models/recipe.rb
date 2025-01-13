8adb1cec9a43c1077ee46f006965d94d14b61e3b
Hollie Jaques <holliejaques@gmail.com>
2025-01-13 18:57:57 +0700

    created routes, controller and views for categories

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,5 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
-  has_many :categories
   validates :description, presence: true
   validates :name, uniqueness: true
   validates :rating, inclusion: { in: 0..10 }
