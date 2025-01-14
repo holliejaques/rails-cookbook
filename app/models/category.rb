8a16736955361fe8193a80538f4189416cdabb46
Hollie Jaques <holliejaques@gmail.com>
2025-01-15 01:33:38 +0700

    cloudinary configuration

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :recipes, through: :bookmarks
   validates :name, uniqueness: true
+  has_one_attached :photo
 end
